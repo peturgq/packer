1f0c6d795487b765f124259e3b7ed54c56945a0e
soffi <soffanias@gmail.com>
2015-06-23 17:16:55 +0000

    re-add user, root instead of ubuntu

--- soffi-debian81/scripts/cloudstack.sh
+++ soffi-debian81/scripts/cloudstack.sh
@@ -1,55 +1,24 @@
-#!/bin/bash
-
 apt-get update
-apt-get upgrade -y
-apt-get install -y acpid ntp inetutils-ping locate screen
-dpkg-reconfigure ntp
 
-echo "acpiphp" >> /etc/modules
-echo "sh -c 'setterm -blank 0 -powersave off -powerdown 0 < /dev/console > /dev/console 2>&1'" >> /etc/rc.local
+# install deps for ubuntu cloud-init package
+apt-get install -y cloud-utils dh-python distro-info distro-info-data euca2ools libmpdec2 libpython3-stdlib libpython3.4-minimal libpython3.4-stdlib libxslt1.1 python-cffi python-crypto python-cryptography python-distro-info python-ecdsa python-lxml python-ndg-httpsclient python-openssl python-paramiko python-ply python-pyasn1 python-pycparser python-requestbuilder python-requests python-setuptools python-urllib3 python-yaml python3 python3-chardet python3-configobj python3-crypto python3-jinja2 python3-json-pointer python3-jsonpatch python3-jwt python3-markupsafe python3-minimal python3-oauthlib python3-pkg-resources python3-prettytable python3-requests python3-serial python3-six python3-urllib3 python3-yaml python3.4 python3.4-minimal cloud-utils dh-python distro-info distro-info-data euca2ools libmpdec2 libpython3-stdlib libpython3.4-minimal libpython3.4-stdlib libxslt1.1 python-cffi python-crypto python-cryptography python-distro-info python-ecdsa python-lxml python-ndg-httpsclient python-openssl python-paramiko python-ply python-pyasn1 python-pycparser python-requestbuilder python-requests python-setuptools python-urllib3 python-yaml python3 python3-chardet python3-configobj python3-crypto python3-jinja2 python3-json-pointer python3-jsonpatch python3-jwt python3-markupsafe python3-minimal python3-oauthlib python3-pkg-resources python3-prettytable python3-requests python3-serial python3-six python3-urllib3 python3-yaml python3.4 python3.4-minimal
 
-rm -f /etc/udev/rules.d/70*
-rm -f /var/lib/dhcp/dhclient.*
+# install cloud-init 0.7.7 from ubuntu while not packaged for debian8, later this can be done normally
+curl -O http://launchpadlibrarian.net/208765957/cloud-init_0.7.7~bzr1091-0ubuntu3_all.deb
+dpkg -i cloud-init_0.7.7~bzr1091-0ubuntu3_all.deb
 
-rm -f /etc/ssh/*key*
+cat <<EOF > /etc/cloud/cloud.cfg.d/99_cloudstack.cfg
+datasource:
+  CloudStack: {}
+  None: {}
+datasource_list:
+  - CloudStack
+EOF
 
-cat /dev/null > /var/log/wtmp 2>/dev/null
-logrotate -f /etc/logrotate.conf 2>/dev/null
-rm -f /var/log/*-* /var/log/*.gz 2>/dev/null
-
-#This is done because otherwise the script that sets up the instance hostname will not run.
-hostname localhost
-rm /etc/hostname
-touch /etc/hostname
-echo "localhost" > /etc/hostname
-
-#Force user to change the root password
-#passwd --expire root
-
-#Set up the init scripts
-wget http://s.greenqloud.com/template-scripts/debian/7/gq-create-ssh-keys -O /etc/init.d/gq-create-ssh-keys
-wget http://s.greenqloud.com/template-scripts/debian/7/baremetal/gq-get-passwd -O /etc/init.d/gq-get-passwd
-wget http://s.greenqloud.com/template-scripts/debian/7/baremetal/gq-get-ssh-keys -O /etc/init.d/gq-get-ssh-keys
-wget http://s.greenqloud.com/template-scripts/debian/7/baremetal/gq-get-userdata -O /etc/init.d/gq-get-userdata
-wget http://s.greenqloud.com/template-scripts/debian/7/hostname -O /etc/dhcp/dhclient-exit-hooks.d/hostname
-
-chmod 755 /etc/init.d/gq-create-ssh-keys
-chmod 755 /etc/init.d/gq-get-passwd
-chmod 755 /etc/init.d/gq-get-ssh-keys
-chmod 755 /etc/init.d/gq-get-userdata
-chmod 755 /etc/dhcp/dhclient-exit-hooks.d/hostname
-
-ln -s /etc/init.d/gq-create-ssh-keys /etc/rc2.d/S18gq-create-ssh-keys
-ln -s /etc/init.d/gq-get-passwd /etc/rc2.d/S18gq-get-passwd
-ln -s /etc/init.d/gq-get-ssh-keys /etc/rc2.d/S18gq-get-ssh-keys 
-ln -s /etc/init.d/gq-get-userdata /etc/rc2.d/S18gq-get-userdata
-
-rm /root/.bash_history
-rm /root/.lesshst
-rm /root/.aptitude/config 
-rmdir /root/.aptitude
-rm /root/.bash_history 
-rm /root/.viminfo 
-
-history -c
-
+# remove ubuntu'isms from cloud.cfg
+sed -i 's/set-passwords/[ set-passwords, always ]/' /etc/cloud/cloud.cfg
+sed -i 's/lock_passwd: True/lock_passwd: False/' /etc/cloud/cloud.cfg
+#sed -i 's/ - default//' /etc/cloud/cloud.cfg
+#sed -i 's/users://' /etc/cloud/cloud.cfg
+sed -i 's/name: ubuntu/name: root/' /etc/cloud/cloud.cfg
+sed -i 's/disable_root: true/disable_root: false/' /etc/cloud/cloud.cfg 
